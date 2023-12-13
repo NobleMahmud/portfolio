b8649389cf67b2865404ccb67afd6dd251d10ac7
NobleMahmud <mahmud.oneis1@gmail.com>
2023-12-14 02:56:31 +0600

    glowing effect added

--- src/pages/Home/HeroSection/HeroSection.jsx
+++ src/pages/Home/HeroSection/HeroSection.jsx
@@ -5,9 +5,13 @@ const HeroSection = () => {
 
         <div className="bg-white min-h-screen flex flex-col-reverse md:flex-row items-center justify-center px-16">
             <div className="relative w-full max-w-lg">
-                <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob "></div>
+                <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-700 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-pulse"></div>
+                <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-700 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-pulse animation-delay-2000"></div>
+                <div className="absolute -bottom-32 left-20 w-72 h-72 bg-purple-700 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-pulse animation-delay-4000"></div>
+
+                {/* <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob "></div>
                 <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-                <div className="absolute -bottom-32 left-20 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+                <div className="absolute -bottom-32 left-20 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div> */}
                 <div className=''>
                     <div className="m-8 relative space-y-4">
                         <TypeWrite></TypeWrite>
